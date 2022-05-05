690cb6526c3f926fb43464fedf2b72b74c4bfb7f
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2022-05-05 16:20:43 -0300

    Projeto Finalizado

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -11,15 +11,19 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 @Table(name = "tab_movimentacao")
 public class Movimentacao {
+
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@Column(name = "data_hora")
+	private LocalDateTime dataHora;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-
-	@Column(name = "data_hora")
-	private LocalDateTime dataHora;
 
 	private String descricao;
 
